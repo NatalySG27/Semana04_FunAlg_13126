88da603f102205848f72d211200756d42f34b10c
Nataly <N00462738@upn.pe>
2025-04-16 17:20:53 -0500

    Añadir color al texto

--- Ejercicio 02/Program.cs
+++ Ejercicio 02/Program.cs
@@ -12,13 +12,13 @@ namespace Ejercicio_02
         {
             Console.WriteLine("Ingrese sus 4 notas para generar su promedio");
             float n1, n2, n3, n4;
-            Console.WriteLine("Ingrese la primera nota: ");
+            Console.WriteLine("Ingrese la primera nota(10%): ");
             n1 = float.Parse(Console.ReadLine());
-            Console.WriteLine("Ingrese la segunda nota: ");
+            Console.WriteLine("Ingrese la segunda nota(20%): ");
             n2 = float.Parse(Console.ReadLine());
-            Console.WriteLine("Ingrese la tercera nota: ");
+            Console.WriteLine("Ingrese la tercera nota(30%): ");
             n3 = float.Parse(Console.ReadLine());
-            Console.WriteLine("Ingrese la cuarta nota: ");
+            Console.WriteLine("Ingrese la cuarta nota(40%): ");
             n4 = float.Parse(Console.ReadLine());
 
             double promedio;
@@ -28,12 +28,15 @@ namespace Ejercicio_02
 
             if (promedio >= 11.5)
             {
+                Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("Estás aprobado");
             }
             else
             {
-                    Console.WriteLine("Estás reprobado"); 
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Estás reprobado"); 
             }
+            Console.ForegroundColor = ConsoleColor.Gray;
             Console.ReadKey();
         }
     }
